238e7928962e3905e87ffb452d2a61036d2736a4
SCN <supercodingninja@outlook.com>
2021-03-20 10:12:40 -0700

    Corrections and different approach.  Need help with this part (speak to tutor).

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
-// I want to make my app operations, here. //
-const db = require('./db');
 const inquirer = require('inquirer');
-const cliQ = require('./assets/library/cliQuestions');
-// * The command-line application should allow users to: Add departments, roles, employees, * View departments, roles, employees, * Update employee roles. //
+const connect = require('./config/connect.js');
+const myDB = require('./db');
+const printTable = require('console.table');
+const mysql = require('mysql');
 
+function init() {
+
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'Task',
+            message: 'Please choose a task.',
+            choices: [
+                'Add an employee.',
+                'View all employees.',
+                'Delete an employee (use for termination).',
+                'Delete an employee (use for employee seperation).',
+                'Delete an employee (use for employee changed companies).',
+                'Add a role to an employee.',
+                `Change an employee's role.`,
+                `Change an employee's department.`,
+                `Change an employee's manager.`,
+                'Add a manager.',
+                'View roles (displays employees by their roles).',
+                'View management teams (displays manager(s), and their team of employees).',
+                'Remove a manager.',
+                'Add a department.',
+                'View departments.',
+                'Remove a department (department obsolete/terminated).',
+                'Remove a role (role obsolete/terminated).',
+                'Quit Application.'
+            ]
+        })
+        
+};
+
+init();
--- config/orm.js
+++ config/orm.js
@@ -2,10 +2,70 @@ const connection = require('./connect.js');
 const app = require('../app.js');
 const {findDpt, findRole, findEmp} = require('../assets/library/cliQuestions.js');
 
+const MySQL = require('string');
+
+module.exports = {
+    
+    findDpt(results) {
+        return connection.query(
+            'INSERT INTO department SET ?', results
+        );
+    },
+
+    findRole(results) {
+        return connection.query(
+            'INSERT INTO role SET ?', results
+        );
+    },
+
+    findEmp(results) {
+        return connection.query(
+            'INSERT INTO employee SET ?', results
+            );
+    },
+
+    seeDpts() {
+        return connection.query('SELECT id, name AS department FROM department');
+    },
+
+    seeRoles() {
+        return connection.query(SQL
+            `SELECT id,
+            title,
+            salary, department.name`);
+    },
+
+    seeEmps() {
+        return connection.query(SQL
+            `SELECT id, 
+            first_name,
+            last_name,
+            title,
+            salary,`);
+    },
+
+    updateRole(results) {
+        return connection.query('UPDATE employee SET ? WHERE ?',
+        [
+            {id: results.id},
+            
+            {id: results.role_id}
+        ])
+    }
+};
+
 findDpt();
 
 findRole();
 
 findEmp();
+
+seeDpts();
+
+seeRoles();
+
+seeEmps();
+
+updateRole(results);
 
 export default seekTables;
