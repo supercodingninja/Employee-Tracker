30dffe8aea7185a1da2051348196551d35de492b
SCN <supercodingninja@outlook.com>
2021-03-20 02:57:15 -0700

    Made some  declarations by functions, while trying to figure out the query for the employee's name.  I think I can make it easy for myself if I reuse some information.

--- assets/library/cliQuestions.js
+++ assets/library/cliQuestions.js
@@ -1,5 +1,50 @@
 const myDB = require ('./config/orm.js'); // I NEED TO CHECK THIS THOUGHT, AND ROUTE. //
 
+// Declarations by functions. //
+// Employee. //
+const employee = () => {
+    return {
+        name:'Employee',
+        type: 'list',
+        message: 'Select an employee.',
+        choices: () => findEmployee ()
+    }
+};
+
+// List of Roles. //
+const findRole = async() =>{
+    
+    var role = await db.seekTables('Roles');
+    
+    return role.map(({role_id, title}) => {
+       
+        return {
+           value: role_id,
+           name: title,
+       }
+    })
+};
+
+// Employee's Role. //
+const empRole = () =>{
+    return {
+        name: 'role_id',
+        type: 'list',
+        message: `What is the employee's role?`,
+        choices: () => findRole ()
+    }
+};
+
+// Finding the employee's manager. //
+const findManager = () => {
+    return {
+        name:'management',
+        type: 'list',
+        message: `Who is the employee's manager`,
+        choices: () => findEmployee ()
+    }
+}
+
 module.exports = {
     // CLI Initial Query. //
     initApp () {
@@ -28,5 +73,37 @@ module.exports = {
                 'Quit Application.'
             ]
         }]
-    }
+    },
+
+    // Employee's Name. //
+    empQ () {
+        return[
+            {
+                name: 'first_name',
+                type: 'input',
+                message: `What is the employee's first name?`
+            },
+
+            {
+                name: 'last_name',
+                type: 'input',
+                message: `What is the employee's last name?`
+            },
+            
+            {
+                name: 'under_management',
+                type: 'input',
+                message: 'Does the employee have a/several manager(s)?',
+                choices: [
+                    'Yes',
+                    'No',
+                    'N/A'
+                ]
+            },
+
+            empRole(), // Use this declaration to find the role. //
+            
+            findManager()
+        ]
+    },
 };
