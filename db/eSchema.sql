c18d3de457002421ae32899c10daa64ba4910563
SCN <supercodingninja@outlook.com>
2021-03-20 00:49:18 -0700

    Completed Schema and Seed.

--- db/eSchema.sql
+++ db/eSchema.sql
@@ -4,38 +4,38 @@ DROP DATABASE IF EXISTS eTrack_db;
 CREATE DATABASE eTrack_db;
 
 USE eTrack_db;
--- -- Design the following database schema containing three tables (see schema.png):
+-- Design the following database schema containing three tables (see schema.png) --
 
--- * **department**:
+-- * **department** --
 CREATE TABLE department(
-    --   * **id** - INT PRIMARY KEY
-    department_id INT PRIMARY KEY,
-    --   * **name** - VARCHAR(30) to hold department name
+    --   * **id** - INT PRIMARY KEY --
+    id INT PRIMARY KEY,
+    --   * **name** - VARCHAR(30) to hold department name. --
     department_name VARCHAR(30)
 );
 
--- * **role**:
+-- * **role** --
 CREATE TABLE role (
-    --   * **id** - INT PRIMARY KEY
-    role_id INT PRIMARY KEY,
-    --   * **title** -  VARCHAR(30) to hold role title
+    --   * **id** - INT PRIMARY KEY --
+    id INT PRIMARY KEY,
+    --   * **title** -  VARCHAR(30) to hold role title. --
     title VARCHAR(30),
-    --   * **salary** -  DECIMAL to hold role salary
-    salary DECIMAL,
-    --   * **department_id** -  INT to hold reference to department role belongs to
+    --   * **salary** -  DECIMAL to hold role salary (Ref. https://stackoverflow.com/questions/4834390/how-to-use-mysql-decimal) --
+    salary DECIMAL (9,2),
+    --   * **department_id** -  INT to hold reference to department role belongs to. --
     department_id INT
 );
 
--- * **employee**:
+-- * **employee** --
 CREATE TABLE employee (
-    --   * **id** - INT PRIMARY KEY
-    employee_id INT PRIMARY KEY,
-    --   * **first_name** - VARCHAR(30) to hold employee first name
+    --   * **id** - INT PRIMARY KEY --
+    id INT PRIMARY KEY,
+    --   * **first_name** - VARCHAR(30) to hold employee first name --
    first_name VARCHAR(30),
-    --   * **last_name** - VARCHAR(30) to hold employee last name
+    --   * **last_name** - VARCHAR(30) to hold employee last name --
     last_name VARCHAR(30),
-    --   * **role_id** - INT to hold reference to role employee has
+    --   * **role_id** - INT to hold reference to role employee has. --
     role_id INT,
-    --   * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+    --   * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager. --
     manager_id INT
 );
--- db/eSeed.sql
+++ db/eSeed.sql
@@ -1,9 +1,40 @@
+-- My thought-flow https://drive.google.com/file/d/1f-RDGCSj9yTMibKaGBSLW9K10wY8RxpY/view; https://drive.google.com/file/d/1AQVuYY_kPW5NQ9Z3i3nZZevw1S7xsrsz/view; and https://drive.google.com/file/d/1GXI_co-9gZFj4ZzhzjWNYzTMkV5VbXnv/view --
 USE eTrack_db;
 
 -- List of Departments --
+INSERT INTO Departments (id, department_name) VALUES
+    (13,'Accounting')
+    (3, 'Business Strategy & Development')
+    (5, 'Design')
+    (1, 'Executive'),
+    (4,' Facilities')
+    (2, 'Finance')
+    (9, 'Human Resources')
+    (8, 'Internal Affairs')
+    (7, 'International Business')
+    (6, 'Legal')
+    (10, 'Marketing & Communications'),
+    (11, 'Operations & Production'),
+    (5, 'Public Relations')
+    (12, 'Purchase (Internal)')
+    (14, 'Recruiting')
+    (15, 'Research & Development'),
+    (16, 'Sales, Services & Support')
 
 
 -- List of Roles --
+INSERT INTO Roles (id, title, salary, departnment_id) VALUES
+    (1,'Fiduciary Accountant', 112000, 1)
+    (2,'Financial Accountant', 91000, 2)
+    (3,'Forensic Accountant', 86267, 8)
+    (4,'Managerial Accountant', 75000, 13)
+    (5,'Cost Accountant', 63000, 13)
 
 
 -- List of Employees Names --
+INSERT INTO Employee (id, first_name, last_name, role_id, manager_id) VALUES
+    (1,'Dwayne', 'Thomas', 1, 1)
+    (2,'Anjelah', 'Johnson-Reyes', 1, 2)
+    (3,'Jeri', 'Ryan', 1, Null)
+    (4,'Bruno', 'Mars', 1, 3)
+    (5,'ZZ', 'Top', 1, Null)
