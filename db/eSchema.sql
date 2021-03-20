54dde87283229cc73ce4063d66e93ddd7126e4f2
SCN <supercodingninja@outlook.com>
2021-03-19 19:54:24 -0700

    Finish Creating my tables for my schema; and included two videos showing my thought-flow.

--- db/eSchema.sql
+++ db/eSchema.sql
@@ -1,3 +1,4 @@
+-- See my thought-flow in how I created this schema at https://drive.google.com/file/d/1mPa_gXlxk7pZr4Ks3kBNk9Atakoykmsp/view; and https://drive.google.com/file/d/145_rDjGIJZWTxObi5AFE6oSGq3sNgbEH/view --
 DROP DATABASE IF EXISTS eTrack_db;
 
 CREATE DATABASE eTrack_db;
@@ -10,7 +11,7 @@ CREATE TABLE department(
     --   * **id** - INT PRIMARY KEY
     department_id INT PRIMARY KEY,
     --   * **name** - VARCHAR(30) to hold department name
-    department_name VARCHAR(30),
+    department_name VARCHAR(30)
 );
 
 -- * **role**:
@@ -18,17 +19,23 @@ CREATE TABLE role (
     --   * **id** - INT PRIMARY KEY
     role_id INT PRIMARY KEY,
     --   * **title** -  VARCHAR(30) to hold role title
-    role_title VARCHAR(30),
+    title VARCHAR(30),
     --   * **salary** -  DECIMAL to hold role salary
-    role_salary DECIMAL
+    salary DECIMAL,
     --   * **department_id** -  INT to hold reference to department role belongs to
     department_id INT
 )
 
 -- * **employee**:
-
---   * **id** - INT PRIMARY KEY
---   * **first_name** - VARCHAR(30) to hold employee first name
---   * **last_name** - VARCHAR(30) to hold employee last name
---   * **role_id** - INT to hold reference to role employee has
---   * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+CREATE TABLE employee (
+    --   * **id** - INT PRIMARY KEY
+    employee_id INT PRIMARY KEY,
+    --   * **first_name** - VARCHAR(30) to hold employee first name
+   first_name VARCHAR(30),
+    --   * **last_name** - VARCHAR(30) to hold employee last name
+    last_name VARCHAR(30),
+    --   * **role_id** - INT to hold reference to role employee has
+    role_id INT,
+    --   * **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+    manager_id INT
+)
