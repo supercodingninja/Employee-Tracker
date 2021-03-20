7e8778a296af5e2acf315b0a3e43f61e215db6bb
SCN <supercodingninja@outlook.com>
2021-03-20 02:09:44 -0700

    Completed CLI Initial Query.

--- assets/library/cliQuestions.js
+++ assets/library/cliQuestions.js
@@ -1,1 +1,32 @@
-// I need to figure out what questions I should be asking (see schema.png) //
+const myDB = require ('./config/orm.js'); // I NEED TO CHECK THIS THOUGHT, AND ROUTE. //
+
+module.exports = {
+    // CLI Initial Query. //
+    initApp () {
+        return [{
+            type: 'list',
+            name: 'action',
+            message: 'Please choose a task.',
+            choices: [
+                'Add an employee.',
+                'View all employees.',
+                'Delete an employee (use for termination).',
+                'Delete an employee (use for employee seperation).',
+                'Delete an employee (use for employee changed companies).',
+                'Add a role to an employee.',
+                `Change an employee's role.`,
+                `Change an employee's department.`,
+                `Change an employee's manager.`,
+                'Add a manager.',
+                'View roles (displays employees by their roles).',
+                'View management teams (displays manager(s), and their team of employees).',
+                'Remove a manager.',
+                'Add a department.',
+                'View departments.',
+                'Remove a department (department obsolete/terminated).',
+                'Remove a role (role obsolete/terminated).',
+                'Quit Application.'
+            ]
+        }]
+    }
+};
