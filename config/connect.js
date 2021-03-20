c2ab5298e2f8b830b682737c953a281db582a99b
SCN <supercodingninja@outlook.com>
2021-03-20 07:40:16 -0700

    Connected MySQL.

--- config/connect.js
+++ config/connect.js
@@ -1,1 +1,21 @@
-// What do I need to require; and I need connect to MySQL, here (see EDB, and team generator). //
+const util = require('util');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'eTrack_db;'
+});
+
+connection.connect((err)=>{
+    
+    if(err) throw err;
+    
+    console.log('This is my connection to MySQL, by id.' + connection.threadId);
+});
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
